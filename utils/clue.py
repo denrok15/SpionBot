74372e6fdbcb2d3473aefab3a497a3d95fcb8ff5
Руслан <stla7824@gmail.com>
2025-12-24 22:39:39 +0700

    поменял названия игр

--- utils/clue.py
+++ utils/clue.py
@@ -5,19 +5,19 @@ from typing import Literal
 class UserClue:
     def __init__(self):
         self.clue_dota2 = {}
-        self.clue_clash_royale = {}
+        self.clue_clashroyale = {}
         self.clues = {
             "dota2": self.clue_dota2,
-            "clash_royale": self.clue_clash_royale,
+            "clashroyale": self.clue_clashroyale,
         }
 
     def found_clue(
         self,
-        game: Literal["dota2", "clash_royale"],
+        game: Literal["dota2", "clashroyale"],
         hero: str,  # название героев нужно брать строго из CONST.PY
         complexity: Literal["easy", "medium", "hard"],
     ):
-        number = random.randint(0, 5)
+        number = random.randint(0, 4)
         return self.clues[game][hero][complexity][number]
 
 
